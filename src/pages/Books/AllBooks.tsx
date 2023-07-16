d8f22d9c49959bc225f114a0b2c0d38409db4300
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-07-16 12:58:51 +0600

    Add New Book Button Added into All Books Page

--- src/pages/Books/AllBooks.tsx
+++ src/pages/Books/AllBooks.tsx
@@ -1,3 +1,4 @@
+import { MdOutlineAddCircleOutline } from 'react-icons/md';
 import BookCard from '../../components/BookCard';
 import { useGetAllBooksQuery } from '../../redux/features/books/booksApi';
 import { IBooks } from '../../types/BookTypes';
@@ -10,7 +11,15 @@ const AllBooks = () => {
     const books = data.data;
     return (
       <div>
-        <h2 className="text-4xl mb-10 font-serif">All Books List</h2>
+        <div className="flex items-center justify-between mb-10">
+          <h2 className="text-4xl font-serif">All Books List</h2>
+          <button
+            className={`py-2 px-4 text-sm bg-[#5870f9] text-white rounded font-semibold font-serif flex items-center gap-1`}
+          >
+            <span>Add New Book</span>
+            <MdOutlineAddCircleOutline className="text-lg" />
+          </button>
+        </div>
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
           {books.map((book: IBooks) => (
             <BookCard book={book} key={book._id} />
